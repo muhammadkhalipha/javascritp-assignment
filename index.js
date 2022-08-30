0dd20654a2de739287cd9209a69995a93a055e74
muhammadkhalipha <yusufmuhammadbiodun@gmail.com>
2022-08-30 06:50:21 +0100

    init

---
+++ index.js
@@ -0,0 +1,32 @@
+// // document.getElementById("body:background-color").innerHTML = "black";
+// function myFunc() {
+//   //   document.getElementById("").style.color = "yellow";
+// }
+// function changeBackground(color) {
+//   document.body.style.background = color;
+// }
+
+// window.addEventListener("load", function () {
+//   changeBackground("red");
+// });
+// console.log(changeBackground);
+// const my_button = document.getElementById("my_button");
+// my_button.addEventListener("click"),
+//   function onClick(event) {
+//     document.body.style.backgroundColor = "Black";
+//   };
+
+// const btn = document.getElementById("btn");
+
+// btn.addEventListener("click"),
+//   function onClick(event) {
+//     // 👇️ change background color
+//     document.body.style.backgroundColor = "white";
+//   };
+
+// function myFunction() {
+//   document.getElementById("body").style.backgroundColor = "yellow";
+// }
+function myFunction() {
+  document.body.style.backgroundColor = "red";
+}
